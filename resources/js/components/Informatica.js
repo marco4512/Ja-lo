d77a621e5f2793457e686ca2057bc3c791e71b6e
marco4512 <marco_alex45@hotmail.com>
2021-06-03 18:54:18 -0500

    Primera Version1.4

--- resources/js/components/Informatica.js
+++ resources/js/components/Informatica.js
@@ -36,15 +36,7 @@ const Informatica =()=> {
          </Form.Group>
           </Form>
          </section>
-  <Table striped bordered hover variant="dark" responsive className="ContenidoTAbla">
-
-  <thead>
-        <tr>
-        <th>Muestras de su trabajo</th>
-        <th>Datos de contacto</th>
-        </tr>
-    </thead>
-    <tbody  className="datos">
+    
     {ListaImagenes.filter((dataItem)=> {
      if(term == ""){
          return dataItem
@@ -60,8 +52,8 @@ const Informatica =()=> {
         
      
     }).map(dataItem =>(
-    <tr>
-        <td md='8' key={dataItem.id} className="ImagenTabla">{
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top">{
             <Carousel>
                 <Carousel.Item >
                        <img key={dataItem.Foto1} 
@@ -85,41 +77,17 @@ const Informatica =()=> {
                        />
                 </Carousel.Item>
             </Carousel>}
-        </td>
-        <tr className="datos" >
-        <th>Nombre</th>    
-        <td key={dataItem.trabajador}>{dataItem.trabajador+' '+dataItem.apellido}</td>
-        <th>Telefono</th>    
-        <td key={dataItem.telefono}>{dataItem.telefono}</td>
-        </tr>
-        <tr className="datos">
-        <th>Ubicación</th>    
-        <td key={dataItem.Estado}>{dataItem.Estado+' '+dataItem.Municipio}</td>
-        <th>Colonia</th>    
-        <td key={dataItem.Colonia}>{dataItem.Colonia}</td>
-        </tr>
-        <tr className="datos">
-        <th>Descripción</th>    
-        <td key={dataItem.Descripcion}>{dataItem.Descripcion}</td>
-        <th>email</th>    
-        <td key={dataItem.email}>{dataItem.email}</td>
-        </tr>       
-    </tr>
+            </Card.Img>
+        <Card.Body>
+        <Card.Title key={dataItem.trabajador} >{dataItem.trabajador+' '+dataItem.apellido}</Card.Title>
+        <Card.Text key={dataItem.Estado} >
+        {dataItem.Estado+' '+dataItem.Municipio+''+dataItem.Colonia}
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+        </Card>
      ))
     }
-    </tbody>
-</Table>
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
 
     </div>
     );
