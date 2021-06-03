93de7ae49149e979a4694bc946246b1ec5567780
marco4512 <marco_alex45@hotmail.com>
2021-06-02 21:16:56 -0500

    Primera Version1.3

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Categoria extends Model
 {
     use HasFactory;
+    protected $table ='-Categorias';
+    protected $fillable =[
+        
+    ];
+    public $timestamps = false;
 }
