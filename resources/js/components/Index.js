b989edbbddd5c4724b2ac5907c4585be2796efc5
marco4512 <marco_alex45@hotmail.com>
2021-06-03 11:01:30 -0500

    Primera Version1.4

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -36,7 +36,7 @@ const Example =()=> {
             <Navbar.Brand as ={Link} to="/Home"><img className="LogoI" src="https://ja-lo.herokuapp.com/images/Logo3.png" ></img></Navbar.Brand>
             <span className="Menu-Icon"><img  src="https://ja-lo.herokuapp.com/images/menu_white_24dp.svg" ></img></span> 
                 <Nav className="mr-auto uln">
-                <Nav.Link as ={Link} to="/Registro" className="uln uli Menu"> Registrat</Nav.Link>
+                <Nav.Link as ={Link} to="/Registro" className="uln uli Menu"> Registrate</Nav.Link>
                 <Nav.Link href="#features" className="uln uli Menu">
                         <Dropdown>
                         <Dropdown.Toggle variant="success" id="dropdown-basic" className="Des" >
