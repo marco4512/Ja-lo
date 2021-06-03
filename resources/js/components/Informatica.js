6070fe28db794ee795968699e2c5f13d57c5a685
marco4512 <marco_alex45@hotmail.com>
2021-06-03 18:21:02 -0500

    Primera Version1.4

--- resources/js/components/Informatica.js
+++ resources/js/components/Informatica.js
@@ -6,6 +6,7 @@ import Col from 'react-bootstrap/Col';
 import FormControl from 'react-bootstrap/FormControl';
 import Axios from 'axios'
 import Table from 'react-bootstrap/Table';
+import Card from 'react-bootstrap/Card'
 const Informatica =()=> {
     const [ListaImagenes, setListaImagenes] = useState([]);
     const[term,setTerm]=useState("");
@@ -108,6 +109,17 @@ const Informatica =()=> {
     }
     </tbody>
 </Table>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
 
     </div>
     );
